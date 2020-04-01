03f50709b5d4a36cd8f271d7bfab11fbb35f8029
David <dalau6@gmail.com>
2020-04-01 00:27:21 -0700

    Check for invalid arguments.

--- bot.js
+++ bot.js
@@ -1,34 +1,3 @@
-const eris = require('eris');
-
-const PREFIX = 'pb!';
-const BOT_OWNER_ID = '523407722880827415';
-const PREMIUM_CUTOFF = 10;
-
-const bot = new eris.Client('my_token');
-
-const premiumRole = {
-   name: 'Premium Member',
-   color: 0x6aa84f,
-   hoist: true, // Show users with this role in their own section of the member list.
-};
-
-async function updateMemberRoleForDonation(guild, member, donationAmount) {
-   // If the user donated more than $10, give them the premium role.
-   if (guild && member && donationAmount >= PREMIUM_CUTOFF) {
-       // Get the role, or if it doesn't exist, create it.
-       let role = Array.from(guild.roles.values())
-           .find(role => role.name === premiumRole.name);
-
-       if (!role) {
-           role = await guild.createRole(premiumRole);
-       }
-
-       // Add the role to the user, along with an explanation
-       // for the guild log (the "audit log").
-       return member.addRole(role.id, 'Donated $10 or more.');
-   }
-}
-
 const commandForName = {};
 commandForName['addpayment'] = {
    botOwnerOnly: true,
@@ -39,9 +8,15 @@ commandForName['addpayment'] = {
        const userId = mention.replace(/<@(.*?)>/, (match, group1) => group1);
        const member = guild.members.get(userId);
 
-       // TODO: Handle invalid command arguments, such as:
-       // 1. No mention or invalid mention.
-       // 2. No amount or invalid amount.
+       const userIsInGuild = !!member;
+       if (!userIsInGuild) {
+           return msg.channel.createMessage('User not found in this guild.');
+       }
+
+       const amountIsValid = amount && !Number.isNaN(amount);
+       if (!amountIsValid) {
+           return msg.channel.createMessage('Invalid donation amount');
+       }
 
        return Promise.all([
            msg.channel.createMessage(`${mention} paid $${amount.toFixed(2)}`),
