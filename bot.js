e50aab3d1446eff7f4aea1c728d043d21dac0430
David <dalau6@gmail.com>
2020-04-01 00:26:43 -0700

    Updated member roles.

--- bot.js
+++ bot.js
@@ -1,71 +1,51 @@
 const eris = require('eris');
 
 const PREFIX = 'pb!';
-const BOT_OWNER_ID = '123456789';
+const BOT_OWNER_ID = '523407722880827415';
+const PREMIUM_CUTOFF = 10;
 
 const bot = new eris.Client('my_token');
 
-const commandForName = {};
-commandForName['addpayment'] = {
-  botOwnerOnly: true,
-  execute: (msg, args) => {
-      const mention = args[0];
-      const amount = parseFloat(args[1]);
-
-      // TODO: Handle invalid command arguments, such as:
-      // 1. No mention or invalid mention.
-      // 2. No amount or invalid amount.
-
-      return msg.channel.createMessage(`${mention} paid $${amount.toFixed(2)}`);
-  },
+const premiumRole = {
+   name: 'Premium Member',
+   color: 0x6aa84f,
+   hoist: true, // Show users with this role in their own section of the member list.
 };
 
-bot.on('messageCreate', async (msg) => {
-  try {
-      const content = msg.content;
+async function updateMemberRoleForDonation(guild, member, donationAmount) {
+   // If the user donated more than $10, give them the premium role.
+   if (guild && member && donationAmount >= PREMIUM_CUTOFF) {
+       // Get the role, or if it doesn't exist, create it.
+       let role = Array.from(guild.roles.values())
+           .find(role => role.name === premiumRole.name);
 
-      // Ignore any messages sent as direct messages.
-      // The bot will only accept commands issued in
-      // a guild.
-      if (!msg.channel.guild) {
-          return;
-      }
+       if (!role) {
+           role = await guild.createRole(premiumRole);
+       }
 
-      // Ignore any message that doesn't start with the correct prefix.
-      if (!content.startsWith(PREFIX)) {
-          return;
-      }
+       // Add the role to the user, along with an explanation
+       // for the guild log (the "audit log").
+       return member.addRole(role.id, 'Donated $10 or more.');
+   }
+}
 
-      // Extract the parts and name of the command
-      const parts = content.split(' ').map(s => s.trim()).filter(s => s);
-      const commandName = parts[0].substr(PREFIX.length);
+const commandForName = {};
+commandForName['addpayment'] = {
+   botOwnerOnly: true,
+   execute: (msg, args) => {
+       const mention = args[0];
+       const amount = parseFloat(args[1]);
+       const guild = msg.channel.guild;
+       const userId = mention.replace(/<@(.*?)>/, (match, group1) => group1);
+       const member = guild.members.get(userId);
 
-      // Get the requested command, if there is one.
-      const command = commandForName[commandName];
-      if (!command) {
-          return;
-      }
+       // TODO: Handle invalid command arguments, such as:
+       // 1. No mention or invalid mention.
+       // 2. No amount or invalid amount.
 
-      // If this command is only for the bot owner, refuse
-      // to execute it for any other user.
-      const authorIsBotOwner = msg.author.id === BOT_OWNER_ID;
-      if (command.botOwnerOnly && !authorIsBotOwner) {
-          return await msg.channel.createMessage('Hey, only my owner can issue that command!');
-      }
-
-      // Separate the command arguments from the command prefix and name.
-      const args = parts.slice(1);
-
-      // Execute the command.
-      await command.execute(msg, args);
-  } catch (err) {
-      console.warn('Error handling message create event');
-      console.warn(err);
-  }
-});
-
-bot.on('error', err => {
- console.warn(err);
-});
-
-bot.connect();
+       return Promise.all([
+           msg.channel.createMessage(`${mention} paid $${amount.toFixed(2)}`),
+           updateMemberRoleForDonation(guild, member, amount),
+       ]);
+   },
+};
